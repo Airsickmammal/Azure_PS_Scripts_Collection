28ed8e75f3e0aa992f646eb8715d8998982f9e69
Airsickmammal <chris.pendlebury@blueyonder.co.uk>
2022-07-20 23:00:32 +0100

    Added Microsoft Online (Azure AD) User Details Query
    Querys Microsoft Azure AD and exports specific field details. 
    As MSOL lacks an "Enabled" field, 2 queries export based on Enabled = True and False

---
+++ Get-MSOL-AZ-AD-Users.ps1
@@ -0,0 +1,63 @@
+﻿###Log Into Azure
+Connect-MsolService
+###Create Arrays for Results
+$Usrs = @()
+###Query Azure AD ##Replace "SEARCH TEXT STRING" With Naming Mask for Target User Principal Accounts
+#Get-MsolUser | gm -MemberType Properties ##Query (Left) Can be Used to Get Column Names For Below Query
+Get-MsolUser -EnabledFilter EnabledOnly | Where-Object {($_.UserPrincipalName -notlike "*SEARCH TEXT STRING*")} |
+%{
+$Usrs += [PSCustomObject] @{ 
+                                            UserPrincipalName = $_.UserPrincipalName;
+                                            DisplayName = $_.DisplayName;
+                                            IsLicensed = $_.IsLicensed;
+                                            LastPasswordChangeTimestamp = $_.LastPasswordChangeTimestamp;
+                                            PasswordNeverExpires = $_.PasswordNeverExpires;
+                                            UserType = $_.UserType;
+                                            WhenCreated = $_.WhenCreated;
+                                            LastDirSyncTime = $_.LastDirSyncTime;
+                                            ##City = $_.City; 
+                                            ##Department = $_.Department;
+                                            ##LicenseAssignmentDetails = $_.LicenseAssignmentDetails;
+                                            ##Licenses = $_.Licenses;
+                                            ##PasswordNeverExpires = $_.PasswordNeverExpires;
+                                            ##PreferredDataLocation = $_.PreferredDataLocation;
+                                            ##ProxyAddresses = $_.ProxyAddresses;
+                                            ##SoftDeletionTimestamp = $_.SoftDeletionTimestamp;
+                                            ##StrongAuthenticationProofupTime = $_.StrongAuthenticationProofupTime;
+                                            ##StrongPasswordRequired = $_.StrongPasswordRequired;
+                                            ##StsRefreshTokenValidFrom = $_.StsRefreshTokenValidFrom;
+                                            ##UsageLocation = $_.UsageLocation;
+                                            ##ValidationStatus = $_.ValidationStatus;
+                                            ##State =$_.State;
+                                            Enabled = "TRUE";
+                                        }}
+
+Get-MsolUser -EnabledFilter DisabledOnly | Where-Object {($_.UserPrincipalName -notlike "*SEARCH TEXT STRING*")} |
+%{
+$Usrs += [PSCustomObject] @{ 
+                                            UserPrincipalName = $_.UserPrincipalName;
+                                            DisplayName = $_.DisplayName;
+                                            IsLicensed = $_.IsLicensed;
+                                            LastPasswordChangeTimestamp = $_.LastPasswordChangeTimestamp;
+                                            PasswordNeverExpires = $_.PasswordNeverExpires;
+                                            UserType = $_.UserType;
+                                            WhenCreated = $_.WhenCreated;
+                                            LastDirSyncTime = $_.LastDirSyncTime;
+                                            ##City = $_.City; 
+                                            ##Department = $_.Department;
+                                            ##LicenseAssignmentDetails = $_.LicenseAssignmentDetails;
+                                            ##Licenses = $_.Licenses;
+                                            ##PasswordNeverExpires = $_.PasswordNeverExpires;
+                                            ##PreferredDataLocation = $_.PreferredDataLocation;
+                                            ##ProxyAddresses = $_.ProxyAddresses;
+                                            ##SoftDeletionTimestamp = $_.SoftDeletionTimestamp;
+                                            ##StrongAuthenticationProofupTime = $_.StrongAuthenticationProofupTime;
+                                            ##StrongPasswordRequired = $_.StrongPasswordRequired;
+                                            ##StsRefreshTokenValidFrom = $_.StsRefreshTokenValidFrom;
+                                            ##UsageLocation = $_.UsageLocation;
+                                            ##ValidationStatus = $_.ValidationStatus;
+                                            ##State =$_.State;
+                                            Enabled = "FALSE";
+                                        }}
+##Export Array to XLSX Excel File ##Replace [LOCATION] With Target Folder For Export File Location                                     
+$Usrs | Sort-Object -Property DisplayName | Export-Excel C:\[LOCATION]\MSOUsersList-$(Get-Date -UFormat "%Y-%m-%d_%H-%M-%S").xlsx
